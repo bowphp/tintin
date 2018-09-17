17b526d8d1c630a1a949f04250cab8f079e86277
Franck Dakia <dakiafranck@gmail.com>
2018-09-17 19:48:05 +0000

    bug fix

--- src/Bow/TintinEngine.php
+++ src/Bow/TintinEngine.php
@@ -30,7 +30,7 @@ class TintinEngine extends EngineAbstract
         $this->template = new Tintin([
             'path' => $config['view.path'],
             'cache' => $config['view.cache'],
-            'extension' => 'tintin.php'
+            'extension' => isset($config['view.extension']) ? $config['view.extension'] : 'tintin.php'
         ]);
     }
 
