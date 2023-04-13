e5b309d50d4fab922e793abb6dcbfc04cec05d80
Franck DAKIA <dakiafranck@gmail.com>
2023-04-13 21:11:10 +0000

    Add the %verbatim tags

---
+++ tests/view/verbatim.tintin.php
@@ -0,0 +1,17 @@
+%verbatim
+    %guest
+        A guest session
+    %endguest
+%endverbatim
+
+%verbatim
+    %if (true)
+        {{{ $name }}}
+    %endif
+%endverbatim
+
+%verbatim
+    %auth
+        A auth session
+    %endauth
+%endverbatim
