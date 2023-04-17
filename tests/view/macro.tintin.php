7208b9ee348d61d24fdaf9bb5d86511ea9e88f75
Franck DAKIA <dakiafranck@gmail.com>
2023-04-17 23:59:11 +0000

    Fixes the custom directives compilation

--- tests/view/macro.tintin.php
+++ tests/view/macro.tintin.php
@@ -1,17 +1,17 @@
 %macro("greeting", string $name = "name")
-    Hello {{ $name }}
+    Hello, {{ $name }}
 %endmacro
 
 %macro("sum", int $number_1, int $number_2)
-    Hello {{ $number_1 + $number_2 }}
+    Sum of {{ $number_1 }} + {{ $number_2 }} = {{ $number_1 + $number_2 }}
 %endmacro
 
 %macro("user_list", $users)
-    <ul>
-        %loop($users as $user)
-            <li>User's {{ $user }}</li>
-        %endloop
-    </ul>
+<ul>
+    %loop($users as $user)
+        <li>User's {{ $user }}</li>
+    %endloop
+</ul>
 %endmacro
 
 %macro('field', $type, $name, $value)
