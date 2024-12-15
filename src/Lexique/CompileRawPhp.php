6ee836854839d7958518d906f0ca777e5b8443b9
Franck DAKIA <dakiafranck@gmail.com>
2024-12-15 12:18:45 +0000

    fix: raw compilation

--- src/Lexique/CompileRawPhp.php
+++ src/Lexique/CompileRawPhp.php
@@ -14,12 +14,56 @@ trait CompileRawPhp
     {
         $expression = trim($expression);
 
+        foreach (['Raw', 'EndRaw'] as $token) {
+            $out = $this->{'compile' . $token}($expression);
+
+            if (strlen($out) !== 0) {
+                $expression = $out;
+            }
+        }
+
+        return $expression;
+    }
+
+    /**
+     * Compile the %raw...%endraw directive
+     *
+     * @param string $expression
+     * @return string
+     */
+    protected function compileRaw(string $expression): string
+    {
+        $expression = trim($expression);
+
         $output = preg_replace_callback(
-            '/\n*\%raw\s*(\n*(?:.+?)\n*)\%endraw\n*/m',
+            '/\%raw/',
             function ($match) {
                 array_shift($match);
 
-                return "<?php " . trim($match[0]) . " ?>";
+                return "<?php";
+            },
+            $expression
+        );
+
+        return $output == $expression ? '' : $output;
+    }
+
+    /**
+     * Compile the %raw...%endraw directive
+     *
+     * @param string $expression
+     * @return string
+     */
+    protected function compileEndRaw(string $expression): string
+    {
+        $expression = trim($expression);
+
+        $output = preg_replace_callback(
+            '/\%endraw/',
+            function ($match) {
+                array_shift($match);
+
+                return "?>";
             },
             $expression
         );
