628c194a7ad73956efdeff8623dbd228f54ab58f
Franck DAKIA <dakiafranck@gmail.com>
2025-12-21 00:44:35 +0000

    formatting

--- src/Lexique/CompileHelpers.php
+++ src/Lexique/CompileHelpers.php
@@ -14,10 +14,12 @@ trait CompileHelpers
      */
     protected function compileHelpersStack(string $expression): string
     {
-        foreach ([
+        foreach (
+            [
             "Auth", "Guest", "Lang", "Env", "Csrf", "Flash", "Production", "Trans",
             "HasFlash", "EndHelpers", "Empty", "NotEmpty", "Method", "Service"
-        ] as $token) {
+            ] as $token
+        ) {
             $out = $this->{"compile" . $token}($expression);
             if (strlen($out) !== 0) {
                 $expression = $out;
